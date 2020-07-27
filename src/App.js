3074a1e9c639c7af00b4c7b6a698c84dd79a7776
Jordan Hooper <snow.hoops1@gmail.com>
2020-07-27 14:20:37 +1200

    adding map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  
+  constructor(props){
+    super(props)
+
+    this.todos = [
+      {
+        id: 1,
+        text: 'Meat'
+      },
+      {
+        id: 2,
+        text: 'Fish'
+      },
+      {
+        id: 3,
+        text: 'Veges'
+      },
+      {
+        id: 4,
+        text: 'Milk'
+      }]
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <div className="bars">
+            <i className="fas fa-bars"></i>
+          </div>
+          <div className="checklist">My Shopping List</div>
+        </header>
+        <main>
+        <div className="notes">
+          
+          {
+            this.todo.map((todo)=>{
+
+              return (
+                <div className="line line1">
+                  <div className="note note1">{todo.text}</div>
+                  <div className="checkbox">.</div>
+                </div>
+              )
+            })
+          }
+        
+        </div>
+        </main>
+        <footer>
+          <div className="button">
+            <i className="fas fa-plus-circle"></i>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+  }
 
 export default App;
